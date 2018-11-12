62b94c1d59d04988a41d5602acfbd4dd5b14edbd
NeuroAssassin <wrbrown70@yahoo.com>
2018-11-12 12:58:47 -0500

    Add simon

---
+++ simon/__init__.py
@@ -0,0 +1,4 @@
+from .simon import Simon
+
+def setup(bot):
+    bot.add_cog(Simon(bot))
---
+++ simon/simon.py
@@ -0,0 +1,101 @@
+from redbot.core import commands
+import random
+import asyncio
+
+BaseCog = getattr(commands, "Cog", object)
+
+class Simon(BaseCog):
+    """Play Simon, and guess the write number sequence!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group()
+    async def simon(self, ctx):
+        """Group command for playing Simon"""
+        pass
+
+    @simon.command()
+    async def start(self, ctx):
+        await ctx.send("Starting game...\n**RULES:**\n```1. When you are ready for the sequence, click the green checkmark.\n2. Watch the sequence carefully, then repeat it back into chat.  For example, if the 1 then the 2 changed, I would type 12.\n3. You are given 10 seconds to repeat the sequence.\n4. When waiting for confirmation for next sequence, click the green check within 5 minutes of the bot being ready.\n5. Answer as soon as you can once the bot adds the stop watch emoji.```")
+        board = [
+            [1, 2],
+            [3, 4]
+        ]
+        level = [1, 4]
+        message = await ctx.send("```" + self.print_board(board) + "```")
+        await message.add_reaction("\u2705")
+        await ctx.send("Click the Green Check Reaction when you are ready for the sequence.")
+
+        def check(reaction, user):
+            return (user == ctx.author) and str(reaction.emoji) == "\u2705"
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=300.0)
+        except asyncio.TimeoutError:
+            message.delete()
+            await ctx.send("Game has ended due to no response for starting the next sequence.")
+            return
+        else:
+            message.remove_reaction('\u2705', self.bot.user)
+            message.remove_reaction('\u2705', ctx.author)
+            message.add_reaction('\u26A0')
+            randoms = []
+            for x in range(level[1]):
+                randoms.append(random.randint(1, 4))
+            for x in randoms:
+                if x == 1:
+                    old = board[0][0]
+                    board[0][1] = "*"
+                    message.edit(content="```" + self.print_board(board) + "```")
+                    await asyncio.sleep(level[0])
+                    board[0][0] = old
+                    message.edit(content="```" + self.print_board(board) + "```")
+                elif x == 2:
+                    old = board[0][1]
+                    board[0][1] = "*"
+                    message.edit(content="```" + self.print_board(board) + "```")
+                    await asyncio.sleep(level[0])
+                    board[0][1] = old
+                    message.edit(content="```" + self.print_board(board) + "```")
+                elif x == 3:
+                    old = board[1][0]
+                    board[1][0] = "*"
+                    message.edit(content="```" + self.print_board(board) + "```")
+                    await asyncio.sleep(level[0])
+                    board[1][0] = old
+                    message.edit(content="```" + self.print_board(board) + "```")
+                elif x == 4:
+                    old = board[1][1]
+                    board[1][1] = "*"
+                    message.edit(content="```" + self.print_board(board) + "```")
+                    await asyncio.sleep(level[0])
+                    board[1][1] = old
+                    message.edit(content="```" + self.print_board(board) + "```")
+            message.remove_reaction('\u26A0', self.bot.user)
+            answer = "".join(randoms)
+            await message.add_reaction("\u23F1")
+
+            def check_t(m):
+                return m.author == ctx.author and m.content.isdigit()
+            try:
+                user_answer = await self.bot.wait_for('message', check=check_t, timeout=10.0)
+            except asyncio.TimeoutError:
+                await ctx.send(f"Sorry {ctx.author.mention}!  You took too long to answer.")
+                await message.remove_reaction("\u23F1", self.bot.user)
+            else:
+                if user_answer == answer:
+                    await message.add_reaction('\u1F44D')
+                else:
+                    await message.add_reaction('\u1F6AB')
+                    await ctx.send(f"Sorry, but that was the incorrect pattern.  The pattern was {answer}")
+                    return
+                await ctx.send("Sequence was correct.  Waiting for confirmation for another...")
+                await message.remove_reaction("\u1F44D", self.bot.user)
+                await message.add_reaction("\u2705")
+
+    def print_board(self, board):
+        col_width = max(len(str(word)) for row in board for word in row) + 2  # padding
+        whole_thing = ""
+        for row in board:
+            whole_thing += "".join(str(word).ljust(col_width) for word in row) + '\n'
+        return whole_thing

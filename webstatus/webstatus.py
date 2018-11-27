add9142009279514d3f6667ee18b21f089f5211d
NeuroAssassin <wrbrown70@yahoo.com>
2018-11-27 07:25:44 -0500

    Fix info.jsons, filter string in webstatus

--- webstatus/webstatus.py
+++ webstatus/webstatus.py
@@ -17,6 +17,29 @@ class Webstatus(BaseCog):
     @commands.command()
     async def webstatus(self, ctx, *, company):
         """Uses https://outage.report/ to see if the company/website is down"""
+        # Filter out https's and www's
+        if company.startswith("https://www."):
+            company = company[12:]
+        elif company.startswith("http://www."):
+            company = company[11:]
+        elif company.startswith("https://") and company[8:12] != "www.":
+            company = company[8:]
+        elif company.startswith("http://") and company[7:11] != "www.":
+            company = company[7:]
+        elif company.startswith("www."):
+            company = company[4:]
+
+        # Filter out suffixes
+        if company.endswith(".com"):
+            company = company[:(len(company)-4)]
+        elif company.endswith(".org"):
+            company = company[:(len(company)-4)]
+        elif company.endswith(".net"):
+            company = company[:(len(company)-4)]
+        elif company.endswith(".gov"):
+            company = company[:(len(company)-4)]
+        elif company.endswith(".edu"):
+            company = company[:(len(company)-4)]
         async with aiohttp.ClientSession() as session:
             try:
                 url = "https://outage.report/" + company.replace(' ', '').lower()
@@ -25,6 +48,9 @@ class Webstatus(BaseCog):
                 await ctx.send(f"An error occurred while fetching the status.  Server responded with status code {webpage.status_code}")
                 return
             else:
+                if webpage.status_code != 200:
+                    await ctx.send(f"An error occurred within outage.report.  The site responded with status code {webpage.status_code}")
+                    return
                 soup = BeautifulSoup(webpage, 'html.parser')
                 results = soup.find_all('div', attrs={'class': 'Alert__Div-s1eb33n4-0'})
                 if len(results) == 0:

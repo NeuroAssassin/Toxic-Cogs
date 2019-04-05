5ef82046d3b65811eb91831af1756de34615ca11
Neuro Assassin <wrbrown70@yahoo.com>
2019-04-05 15:18:02 -0400

    [V3 Evolution] Add evolution cog

---
+++ evolution/__init__.py
@@ -0,0 +1,8 @@
+from .evolution import Evolution
+from redbot.core import bank
+
+async def setup(bot):
+    is_global = await bank.is_global()
+    if not is_global:
+        raise RuntimeError("Bank must be global for this cog to work.")
+    bot.add_cog(Evolution(bot))
---
+++ evolution/evolution.py
@@ -0,0 +1,294 @@
+from redbot.core import commands, Config, bank
+from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
+import copy
+import asyncio
+import random
+import discord
+import math
+
+ANIMALS = ["chicken", "dog", "cat", "shark", "tiger", "penguin", "pupper", "dragon"]
+
+LEVELS = {
+    1: {80: 1, 100: 10},
+    2: {60: 1, 100: 10},
+    3: {50: 1, 90: 10, 100: 100},
+    4: {30: 1, 70: 10, 100: 100},
+    5: {10: 1, 60: 10, 100: 100},
+    6: {40: 10, 100: 100},
+    7: {20: 10, 90: 100, 100: 1000},
+    8: {80: 100, 100: 1000},
+    9: {60: 100, 100: 1000},
+    10: {50: 100, 100: 1000},
+    11: {40: 100, 100: 1000},
+    12: {30: 100, 100: 1000},
+    13: {20: 100, 100: 1000},
+    14: {10: 100, 100: 1000},
+    15: {100: 1000},
+}
+
+
+class Evolution(commands.Cog):
+    """EVOLVE THOSE ANIMALS!!!!!!!!!!!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.conf = Config.get_conf(self, identifier=473541068378341376)
+        default_user = {"animal": "", "animals": {}, "multiplier": 1.0, "bought": {}}
+        self.conf.register_user(**default_user)
+        self.money_task = self.bot.loop.create_task(self.bg_task())
+        self.gain_task = self.bot.loop.create_task(self.gain_bg_task())
+
+    def __unload(self):
+        self.money_task.cancel()
+        self.gain_task.cancel()
+
+    async def gain_bg_task(self):
+        await self.bot.wait_until_ready()
+        while True:
+            seen = []
+            for guild in self.bot.guilds:
+                for member in guild.members:
+                    if member.id in seen:
+                        continue
+                    seen.append(member.id)
+                    animals = await self.conf.user(member).animals()
+                    animal = await self.conf.user(member).animal()
+                    if animal == "":
+                        continue
+                    prev = int(animals.get("1", 0))
+                    if prev < 6:
+                        animals["1"] = prev + 1
+                        await self.conf.user(member).animals.set(animals)
+            await asyncio.sleep(600)
+
+    async def bg_task(self):
+        await self.bot.wait_until_ready()
+        while True:
+            seen = []
+            for guild in self.bot.guilds:
+                for member in guild.members:
+                    if member.id in seen:
+                        continue
+                    seen.append(member.id)
+                    animals = await self.conf.user(member).animals()
+                    animal = await self.conf.user(member).animal()
+                    multiplier = await self.conf.user(member).multiplier()
+                    if animal == "":
+                        continue
+                    for key, value in animals.items():
+                        for x in range(0, value):
+                            chance = random.randint(1, 100)
+                            try:
+                                chances = list(LEVELS[int(key)].keys())
+                            except:
+                                chances = [100]
+                            chosen = min([c for c in chances if chance <= c])
+                            try:
+                                gaining = LEVELS[int(key)][chosen]
+                            except:
+                                gaining = 1000
+                            gaining *= multiplier
+                            await bank.deposit_credits(member, math.ceil(gaining))
+            await asyncio.sleep(60)
+
+    def get_level_tax(self, level):
+        if level == 1:
+            return 0
+        return (self.get_level_tax(level - 1) * 2) + 100
+
+    def get_total_price(self, level, bought, amount):
+        normal = level * 300 * amount
+        level_tax = self.get_level_tax(level)
+        tax = bought * 100
+        return normal + level_tax + tax
+
+    async def shop_control_callback(self, ctx, pages, controls, message, page, timeout, emoji):
+        description = message.embeds[0].description
+        level = int(description.split(" ")[1])
+        await ctx.invoke(self.buy, level=level)
+
+    @commands.group(alises=["e", "evo"])
+    async def evolution(self, ctx):
+        """EVOLVE THE GREATEST ANIMALS OF ALL TIME!!!!"""
+        pass
+
+    @evolution.command()
+    async def start(self, ctx):
+        """Start your adventure..."""
+        animal = await self.conf.user(ctx.author).animal()
+        if animal != "":
+            return await ctx.send("You have already started your evolution.")
+        if animal == "P":
+            return await ctx.send("You are starting your evolution.")
+        await self.conf.user(ctx.author).animal.set("P")
+        await ctx.send(
+            f"Hello there.  Welcome to Evolution, where you can buy animals to earn credits for economy.  What would you like your animals to be named (singular please)?  Warning: this cannot be changed.  Here is a list of the current available ones: `{'`, `'.join(ANIMALS)}`"
+        )
+
+        def check(m):
+            return (
+                (m.author.id == ctx.author.id)
+                and (m.channel.id == ctx.channel.id)
+                and (m.content.lower() in ANIMALS)
+            )
+
+        try:
+            message = await self.bot.wait_for("message", check=check, timeout=30.0)
+        except asyncio.TimeoutError:
+            await self.conf.user(ctx.author).animal.set("")
+            return await ctx.send("Command timed out.")
+        await self.conf.user(ctx.author).animal.set(message.content.lower())
+        await ctx.send(
+            f"Your animal has been set to {message.content}.  You have been granted one to start."
+        )
+        await self.conf.user(ctx.author).animals.set({1: 1})
+
+    @evolution.command()
+    async def buy(self, ctx, level: int, amount: int = 1):
+        """Buy those animals to get more economy credits"""
+        animal = await self.conf.user(ctx.author).animal()
+        if animal in ["", "P"]:
+            return await ctx.send("Finish starting your evolution first")
+        animals = await self.conf.user(ctx.author).animals()
+        highest = max(list(animals.keys()))
+        prev = int(animals.get(str(level), 0))
+        balance = await bank.get_balance(ctx.author)
+        bought = await self.conf.user(ctx.author).bought()
+        current_bought = int(bought.get(str(level), 0))
+        if balance < level * 300 * amount:
+            return await ctx.send("You don't have enough credits!")
+        if prev >= 6:
+            return await ctx.send("You have too many of those!  Evolve some of them already.")
+        if prev + amount > 6:
+            return await ctx.send("You'd have too many of those!  Evolve some of them already.")
+        if level < 1:
+            return await ctx.send("Ya cant buy a negative level!")
+        if amount < 1:
+            return await ctx.send("Ya cant buy a negative amount!")
+        if (level > int(highest) - 3) and (level > 1):
+            return await ctx.send("Please get higher animals to buy higher levels of them.")
+        price = self.get_total_price(level, current_bought, amount)
+        m = await ctx.send(
+            f"Are you sure you want to buy {amount} Level {str(level)} {animal}{'s' if amount != 1 else ''}?  This will cost you {str(price)}."
+        )
+        await m.add_reaction("\N{WHITE HEAVY CHECK MARK}")
+        await m.add_reaction("\N{CROSS MARK}")
+
+        def check(reaction, user):
+            return (
+                (user.id == ctx.author.id)
+                and (str(reaction.emoji) in ["\N{WHITE HEAVY CHECK MARK}", "\N{CROSS MARK}"])
+                and (reaction.message.id == m.id)
+            )
+
+        try:
+            reaction, user = await self.bot.wait_for("reaction_add", check=check, timeout=60.0)
+        except asyncio.TimeoutError:
+            return await ctx.send(f"You left the {animal} shop without buying anything.")
+        if str(reaction.emoji) == "\N{CROSS MARK}":
+            return await ctx.send(f"You left the {animal} shop without buying anything.")
+        animals[str(level)] = prev + amount
+        await self.conf.user(ctx.author).animals.set(animals)
+        await bank.withdraw_credits(ctx.author, price)
+        bought[level] = current_bought + 1
+        await self.conf.user(ctx.author).bought.set(bought)
+        await ctx.send(
+            f"You bought {amount} Level {str(level)} {animal}{'s' if amount != 1 else ''}"
+        )
+
+    @evolution.command()
+    async def shop(self, ctx):
+        """View them animals in a nice little buying menu"""
+        animal = await self.conf.user(ctx.author).animal()
+        if animal in ["", "P"]:
+            return await ctx.send("Finish starting your evolution first")
+        animals = await self.conf.user(ctx.author).animals()
+        bought = await self.conf.user(ctx.author).bought()
+        embed_list = []
+        for x in list(animals.keys()):
+            embed = discord.Embed(
+                title=f"{animal.title()} Shop", description=f"Level {str(x)}", color=0xD2B48C
+            )
+            embed.add_field(name="You currently own", value=animals[x])
+            current = int(bought.get(str(x), 0))
+            embed.add_field(name="You have bought", value=current)
+            embed.add_field(name="Price", value=self.get_total_price(int(x), current, 1))
+            last = 0
+            chances = []
+            for chance, value in LEVELS[int(x)].items():
+                chances.append(f"{str(chance-last)}% chance to gain {str(value)}")
+                last = chance
+            embed.add_field(name="Income", value="\n".join(chances))
+            embed_list.append(embed)
+        controls = copy.deepcopy(DEFAULT_CONTROLS)
+        controls["\N{MONEY BAG}"] = self.shop_control_callback
+        await menu(ctx, embed_list, controls)
+
+    @evolution.command()
+    async def backyard(self, ctx):
+        """Where ya animals live!"""
+        animal = await self.conf.user(ctx.author).animal()
+        if animal in ["", "P"]:
+            return await ctx.send("Finish starting your evolution first")
+        embed = discord.Embed(
+            title=f"The amount of {animal}s you have in your backyard.", color=0xD2B48C
+        )
+        animals = await self.conf.user(ctx.author).animals()
+        for level, amount in animals.items():
+            if amount == 0:
+                continue
+            embed.add_field(
+                name=f"Level {str(level)} {animal}",
+                value=f"You have {str(amount)} Level {level} {animal}{'s' if amount != 1 else ''} \N{ZERO WIDTH SPACE} \N{ZERO WIDTH SPACE}",
+            )
+        await ctx.send(embed=embed)
+
+    @evolution.command()
+    async def evolve(self, ctx, level: int, amount: int = 1):
+        """Evolve them animals to get more of da economy credits"""
+        if level < 1 or amount < 1:
+            return await ctx.send("Too low!")
+        animal = await self.conf.user(ctx.author).animal()
+        if animal in ["", "P"]:
+            return await ctx.send("Finish starting your evolution first")
+        animals = await self.conf.user(ctx.author).animals()
+        current = animals.get(str(level), 0)
+        highest = int(max(list(animals.keys())))
+        if current < 2:
+            return await ctx.send("You don't have enough animals at that level.")
+        counter = 0
+        found_new = False
+        recreate = False
+        for x in range(amount):
+            animals[str(level)] -= 2
+            prev = animals.get(str(level + 1), 0)
+            if prev == 6:
+                continue
+            if highest == level:
+                found_new = True
+            animals[str(level + 1)] = animals.get(str(level + 1), 0) + 1
+            await self.conf.user(ctx.author).animals.set(animals)
+            counter += 2
+            if level + 1 == 26:
+                recreate = True
+        if found_new:
+            sending = "\nCONGRATULATIONS!  You have found a new animal!"
+        else:
+            sending = ""
+        await ctx.send(
+            f"Successfully converted {str(counter)} Level {str(level)} {animal}s into {int(counter / 2)} Level {str(level+1)} {animal}{'s' if amount != 1 else ''}{sending}"
+        )
+        if recreate:
+            multiplier = await self.conf.user(ctx.author).multiplier()
+            new = (
+                "**Report:**\n"
+                f"**To:** {ctx.author.display_name}\n"
+                f"**Concerning:** Animal experiment #{str(math.ceil(((multiplier - 1) * 5) + 1))}\n"
+                f"**Subject:** Animal experiment concluded.\n\n"
+                f"Congratulations, {ctx.author.display_name}!  You have successfully combined enough chickens to reach a Level 26 Animal!  This means that it is time to recreate universe!  This will give you a boost of 50,000 credits, remove all of your animals, and give you an extra 0.2% income rate for the next universe from all income.  Congratulations!\n\n"
+                f"From, The Head {animal.title()}"
+            )
+            await ctx.send(new)
+            await bank.deposit_credits(ctx.author, 50000)
+            await self.conf.user(ctx.author).animals.set({1: 1})
+            await self.conf.user(ctx.author).multiplier.set(multiplier + 0.2)

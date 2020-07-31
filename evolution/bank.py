f9fe94ea2cfd38dd4ca5626f4cfb8d47a72f48cf
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-07-31 12:30:18 +0100

    isort formatting agaisnt imports

--- evolution/bank.py
+++ evolution/bank.py
@@ -2,17 +2,15 @@ from __future__ import annotations
 
 import asyncio
 import datetime
-from typing import Union, List, Optional, TYPE_CHECKING
 from functools import wraps
+from typing import TYPE_CHECKING, List, Optional, Union
 
 import discord
+from redbot.core import Config, bank, commands, errors
 from redbot.core.bank import Account
-
-from redbot.core.utils.chat_formatting import humanize_number
-from redbot.core import Config, errors, commands, bank
 from redbot.core.i18n import Translator
-
 from redbot.core.utils import AsyncIter
+from redbot.core.utils.chat_formatting import humanize_number
 
 if TYPE_CHECKING:
     from redbot.core.bot import Red

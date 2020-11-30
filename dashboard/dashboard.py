400b7dd39cd53279e39bad08e0bff12032aaa550
NeuroAssassin <42872277+NeuroAssassin@users.noreply.github.com>
2020-11-30 11:36:29 +0000

    Version bump

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -37,7 +37,7 @@ class Dashboard(
     metaclass=CompositeMetaClass,
 ):
 
-    __version__ = "0.1.6a"
+    __version__ = "0.1.6a.dev1"
 
     def __init__(self, bot: Red, *args, **kwargs):
         super().__init__(*args, **kwargs)
